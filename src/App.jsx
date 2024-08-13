c9f745049127d1341ceea302e0ee6b108a1e3d9a
mdarf <mdarfinji45@gmail.com>
2024-08-13 10:55:47 +0800

    sidebar css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+        <hr />
+
+        <div className="app-content">
+          <Sidebar></Sidebar>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
