ecb8e1bf10559e874f9f9d54b1d2e3f3e34249c6
mdarf <mdarfinji45@gmail.com>
2024-08-13 11:35:12 +0800

    added  product category select

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,8 +1,44 @@
 import React from "react";
 import "./Add.css";
+import { assets } from "../../assets/admin_assets/assets";
 
 const Add = () => {
-  return <div>Add</div>;
+  return (
+    <div className="add">
+      <form className="flex-col">
+        <div className="add-img-upload flex-col">
+          <p>Upload Image</p>
+
+          <label htmlFor="image" className="image">
+            <img src={assets.upload_area} alt="" srcset="" />
+          </label>
+          <input type="file" name="" id="" hidden required />
+        </div>
+        <div className="add-product-name flex-col">
+          <p>Product Name</p>
+
+          <input type="text" name="name" id="" placeholder="Type Here" />
+        </div>
+
+        <div className="add-product-description flex-col">
+          <p>Product Description</p>
+          <textarea
+            name="description"
+            placeholder="write content here"
+            id=""
+            required
+          ></textarea>
+        </div>
+
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product Category</p>
+            <select name="category"></select>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Add;
