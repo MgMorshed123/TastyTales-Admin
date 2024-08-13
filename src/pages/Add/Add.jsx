f34ffc90b67c2dd566c3e2453ee5d243ad24d986
mdarf <mdarfinji45@gmail.com>
2024-08-14 06:31:30 +0800

    added image preview

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/admin_assets/assets";
 
 const Add = () => {
+  const [image, setImage] = useState(false);
+
   return (
     <div className="add">
       <form className="flex-col">
@@ -10,9 +12,21 @@ const Add = () => {
           <p>Upload Image</p>
 
           <label htmlFor="image" className="image">
-            <img src={assets.upload_area} alt="" srcset="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+              srcset=""
+            />
           </label>
-          <input type="file" name="" id="" hidden required />
+          {/* imp */}
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            name=""
+            id=""
+            // hidden
+            required
+          />
         </div>
         <div className="add-product-name flex-col">
           <p>Product Name</p>
@@ -33,9 +47,26 @@ const Add = () => {
         <div className="add-category-price">
           <div className="add-category flex-col">
             <p>Product Category</p>
-            <select name="category"></select>
+            <select name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Deserts">Deserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col">
+            <p>Product Price</p>
+            <input type="number" name="price" placeholder="$20" id="" />
           </div>
         </div>
+        <button className="add-btn" type="submit">
+          {" "}
+          Add
+        </button>
       </form>
     </div>
   );
